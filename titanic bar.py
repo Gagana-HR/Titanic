c3b8e659a87e8ba0717e5ec6e99657be1869911b
Gagana-HR <gaganahr207@gmail.com>
2025-07-24 19:30:07 +0530

    Add files via upload

---
+++ titanic bar.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("train.csv")
+
+class_counts = df['Pclass'].value_counts().sort_index()
+plt.bar(['Class 1', 'Class 2', 'Class 3'], class_counts)
+plt.title('Number of Passengers by Class')
+plt.xlabel('Pclass')
+plt.ylabel('Count')
+plt.show()
